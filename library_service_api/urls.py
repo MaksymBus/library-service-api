3799a01a06f4a1e83d97443b7dd6db1b5bca0a27
Maksym Buslenko <maksbusl@gmail.com>
2025-09-29 14:18:59 +0300

    fix books name url in library-service-api

--- library_service_api/urls.py
+++ library_service_api/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("api/books/", include("books.urls", namespace="books")),
+    path("api/", include("books.urls", namespace="books")),
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
