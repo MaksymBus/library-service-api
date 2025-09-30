550f092fa7874e0bc3c1f41c270fdca473b731c3
Maksym Buslenko <maksbusl@gmail.com>
2025-09-30 19:50:12 +0300

    implement filtering in BorrowingViewSet using is_active and user_id parameters

--- borrowings/views.py
+++ borrowings/views.py
@@ -24,4 +24,21 @@ class BorrowingViewSet(
 
         return BorrowingListSerializer
 
+    def get_queryset(self):
+        is_active = self.request.query_params.get("is_active")
+        current_user = self.request.user
+        queryset = self.queryset
 
+        if not current_user.is_staff:
+            queryset = queryset.filter(user=current_user)
+        else:
+            user_id = self.request.query_params.get("user_id")
+            if user_id:
+                queryset = queryset.filter(user_id=user_id)
+
+        if is_active == "true":
+            queryset = queryset.filter(actual_return_date__isnull=True)
+        if is_active == "false":
+            queryset = queryset.filter(actual_return_date__isnull=False)
+
+        return queryset
